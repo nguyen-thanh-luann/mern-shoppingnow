d3a4b5c1427249f43226cf6ee54edb46d9590580
nguyen-thanh-luann <imthanhluan@gmail.com>
2022-07-12 07:20:23 +0700

    create app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <a href='/'>shoppingnow</a>
+      </header>
+    </div>
+  )
+}
+
+export default App
