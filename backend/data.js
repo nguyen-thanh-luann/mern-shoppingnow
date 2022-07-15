11b8bcb547870e8a886f3429c124bd470bd4bdd3
nguyen-thanh-luann <imthanhluan@gmail.com>
2022-07-15 08:03:35 +0700

    update template

--- backend/data.js
+++ backend/data.js
@@ -1,7 +1,21 @@
+import bcrypt from 'bcryptjs'
 const data = {
+  users: [
+    {
+      name: 'imthanhluan',
+      email: 'imthanhluan@gmail.com',
+      password: bcrypt.hashSync('12345678'),
+      isAdmin: true,
+    },
+    {
+      name: 'thaonguyen',
+      email: 'thaonguyen@gmail.com',
+      password: bcrypt.hashSync('12345678'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
-      _id: '1',
       name: 'Nike Slim shirt',
       slug: 'nike-slim-shirt',
       category: 'Shirts',
@@ -15,7 +29,6 @@ const data = {
       description: 'high quality shirt',
     },
     {
-      _id: '2',
       name: 'Adidas Fit Shirt',
       slug: 'adidas-fit-shirt',
       category: 'Shirts',
@@ -29,7 +42,6 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '3',
       name: 'Nike Slim Pant',
       slug: 'nike-slim-pant',
       category: 'Pants',
@@ -43,7 +55,6 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '4',
       name: 'Adidas Fit Pant',
       slug: 'adidas-fit-pant',
       category: 'Pants',
--- frontend/src/screens/MapScreen.js
+++ frontend/src/screens/MapScreen.js
@@ -1,87 +1,87 @@
-import axios from 'axios';
-import React, { useContext, useEffect, useRef, useState } from 'react';
+import axios from 'axios'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import {
   LoadScript,
   GoogleMap,
   StandaloneSearchBox,
   Marker,
-} from '@react-google-maps/api';
-import { useNavigate } from 'react-router-dom';
-import { Store } from '../Store';
-import Button from 'react-bootstrap/Button';
-import { toast } from 'react-toastify';
+} from '@react-google-maps/api'
+import { useNavigate } from 'react-router-dom'
+import { Store } from '../Store'
+import Button from 'react-bootstrap/Button'
+import { toast } from 'react-toastify'
 
-const defaultLocation = { lat: 45.516, lng: -73.56 };
-const libs = ['places'];
+const defaultLocation = { lat: 45.516, lng: -73.56 }
+const libs = ['places']
 
 export default function MapScreen() {
-  const { state, dispatch: ctxDispatch } = useContext(Store);
-  const { userInfo } = state;
-  const navigate = useNavigate();
-  const [googleApiKey, setGoogleApiKey] = useState('');
-  const [center, setCenter] = useState(defaultLocation);
-  const [location, setLocation] = useState(center);
+  const { state, dispatch: ctxDispatch } = useContext(Store)
+  const { userInfo } = state
+  const navigate = useNavigate()
+  const [googleApiKey, setGoogleApiKey] = useState('')
+  const [center, setCenter] = useState(defaultLocation)
+  const [location, setLocation] = useState(center)
 
-  const mapRef = useRef(null);
-  const placeRef = useRef(null);
-  const markerRef = useRef(null);
+  const mapRef = useRef(null)
+  const placeRef = useRef(null)
+  const markerRef = useRef(null)
 
   const getUserCurrentLocation = () => {
     if (!navigator.geolocation) {
-      alert('Geolocation os not supported by this browser');
+      alert('Geolocation os not supported by this browser')
     } else {
       navigator.geolocation.getCurrentPosition((position) => {
         setCenter({
           lat: position.coords.latitude,
           lng: position.coords.longitude,
-        });
+        })
         setLocation({
           lat: position.coords.latitude,
           lng: position.coords.longitude,
-        });
-      });
+        })
+      })
     }
-  };
+  }
   useEffect(() => {
     const fetch = async () => {
       const { data } = await axios('/api/keys/google', {
         headers: { Authorization: `BEARER ${userInfo.token}` },
-      });
-      setGoogleApiKey(data.key);
-      getUserCurrentLocation();
-    };
+      })
+      setGoogleApiKey(data.key)
+      getUserCurrentLocation()
+    }
 
-    fetch();
+    fetch()
     ctxDispatch({
       type: 'SET_FULLBOX_ON',
-    });
-  }, [ctxDispatch]);
+    })
+  }, [ctxDispatch])
 
   const onLoad = (map) => {
-    mapRef.current = map;
-  };
+    mapRef.current = map
+  }
   const onIdle = () => {
     setLocation({
       lat: mapRef.current.center.lat(),
       lng: mapRef.current.center.lng(),
-    });
-  };
+    })
+  }
 
   const onLoadPlaces = (place) => {
-    placeRef.current = place;
-  };
+    placeRef.current = place
+  }
   const onPlacesChanged = () => {
-    const place = placeRef.current.getPlaces()[0].geometry.location;
-    setCenter({ lat: place.lat(), lng: place.lng() });
-    setLocation({ lat: place.lat(), lng: place.lng() });
-  };
+    const place = placeRef.current.getPlaces()[0].geometry.location
+    setCenter({ lat: place.lat(), lng: place.lng() })
+    setLocation({ lat: place.lat(), lng: place.lng() })
+  }
 
   const onMarkerLoad = (marker) => {
-    markerRef.current = marker;
-  };
+    markerRef.current = marker
+  }
 
   const onConfirm = () => {
-    const places = placeRef.current.getPlaces() || [{}];
+    const places = placeRef.current.getPlaces() || [{}]
     ctxDispatch({
       type: 'SAVE_SHIPPING_ADDRESS_MAP_LOCATION',
       payload: {
@@ -92,15 +92,15 @@ export default function MapScreen() {
         vicinity: places[0].vicinity,
         googleAddressId: places[0].id,
       },
-    });
-    toast.success('location selected successfully.');
-    navigate('/shipping');
-  };
+    })
+    toast.success('location selected successfully.')
+    navigate('/shipping')
+  }
   return (
-    <div className="full-box">
+    <div className='full-box'>
       <LoadScript libraries={libs} googleMapsApiKey={googleApiKey}>
         <GoogleMap
-          id="smaple-map"
+          id='smaple-map'
           mapContainerStyle={{ height: '100%', width: '100%' }}
           center={center}
           zoom={15}
@@ -111,9 +111,9 @@ export default function MapScreen() {
             onLoad={onLoadPlaces}
             onPlacesChanged={onPlacesChanged}
           >
-            <div className="map-input-box">
-              <input type="text" placeholder="Enter your address"></input>
-              <Button type="button" onClick={onConfirm}>
+            <div className='map-input-box'>
+              <input type='text' placeholder='Enter your address'></input>
+              <Button type='button' onClick={onConfirm}>
                 Confirm
               </Button>
             </div>
@@ -122,5 +122,5 @@ export default function MapScreen() {
         </GoogleMap>
       </LoadScript>
     </div>
-  );
+  )
 }
